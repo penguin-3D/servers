5bc77fe90c0e02518cdf93c244c13fde7cd075fc
igorgames-htm <sigmasigma14653@gmail.com>
2025-10-21 21:57:58 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import { WebSocketServer } from 'ws';
+import { WebSocketServer } from "ws";
 
 let players = new Map();
 
@@ -9,20 +9,30 @@ setInterval(() => {
     let data = [];
     players.forEach((value, key) => {
       if (value.showonmap == true) {
-        data.push({id: value.id, username: value.username, position: value.position,rotation: value.rotation});
+        data.push({
+          id: value.id,
+          position: value.position,
+          rotation: value.rotation,
+        });
       }
     });
-    sendallclients(JSON.stringify({type: "update", data: data}));
+    sendallclients(JSON.stringify({ type: "update", data: data }));
   }
 }, 50);
 
-wss.on('connection', ws => {
-  let id = Math.floor(Math.random()*10000);
-  players.set(ws, {showonmap: false, id: id, username: '', position: [0,22,0], rotation: 0});
+wss.on("connection", (ws) => {
+  let id = Math.floor(Math.random() * 10000);
+  players.set(ws, {
+    showonmap: false,
+    id: id,
+    username: "",
+    position: [0, 22, 0],
+    rotation: 0,
+  });
 
-  ws.send(JSON.stringify({type: "id", id: id}));
+  ws.send(JSON.stringify({ type: "id", id: id }));
 
-  ws.on('close', () => {
+  ws.on("close", () => {
     const player = players.get(ws);
     if (!player) return;
     players.delete(ws);
@@ -30,10 +40,10 @@ wss.on('connection', ws => {
       sendallclients(JSON.stringify({ type: "leave", id: player.id }));
     }
   });
-  
-  ws.on('message', message => {
+
+  ws.on("message", (message) => {
     let data = message.toString();
-    
+
     try {
       data = JSON.parse(data);
     } catch (e) {
@@ -44,13 +54,27 @@ wss.on('connection', ws => {
       if (players.get(ws).showonmap == true) {
         return;
       }
-      players.set(ws, {showonmap: true, id: id, username: data.username, position: [0,22,0],rotation: 0});
-      sendallclients(JSON.stringify({type: "join", id: id, username: data.username}));
+      players.set(ws, {
+        showonmap: true,
+        id: id,
+        username: data.username,
+        position: [0, 22, 0],
+        rotation: 0,
+      });
+      sendallclients(
+        JSON.stringify({ type: "join", id: id, username: data.username }),
+      );
     } else if (data.type == "update") {
       if (players.get(ws).showonmap == false) {
         return;
       }
-      players.set(ws, {showonmap: true, id: id, username: players.get(ws).username, position: data.position, rotation: data.rotation});
+      players.set(ws, {
+        showonmap: true,
+        id: id,
+        username: players.get(ws).username,
+        position: data.position,
+        rotation: data.rotation,
+      });
     } else if (data.type == "list") {
       if (players.get(ws).showonmap == false) {
         return;
@@ -58,18 +82,18 @@ wss.on('connection', ws => {
       let data = [];
       players.forEach((value, key) => {
         if (value.showonmap == true) {
-          data.push({id: value.id, username: value.username})
+          data.push({ id: value.id, username: value.username });
         }
       });
-      ws.send(JSON.stringify({type: "list", data: data}));
+      ws.send(JSON.stringify({ type: "list", data: data }));
     } else if (data.type == "ping") {
-      ws.send(JSON.stringify({type: "pong"}));
+      ws.send(JSON.stringify({ type: "pong" }));
     }
   });
 });
 
 function sendallclients(message) {
-  wss.clients.forEach(client => {
+  wss.clients.forEach((client) => {
     if (client.readyState === WebSocket.OPEN) {
       client.send(message);
     }
